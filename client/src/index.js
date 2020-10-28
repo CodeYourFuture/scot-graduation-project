54385da978bd10e9378267583967764e2cbb0200
Rares Matei <matei.rar@gmail.com>
2020-10-28 23:40:43 +0000

    add auth example

--- client/src/index.js
+++ client/src/index.js
@@ -4,37 +4,54 @@ import "./index.css";
 import Home from "./components/Home";
 import About from "./components/About";
 import Status from "./components/Status";
+import Login from "./components/Login";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const logout = e => {
+  e.preventDefault();
+  localStorage.removeItem("token");
+  document.location.reload();
+};
 
 const Routes = () => {
   return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link className="nav-link" to="/">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link className="nav-link" to="/about">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link className="nav-link" to="/status">
-              Status
-            </Link>
-          </li>
-        </ul>
-
+      <Router>
         <div>
-          <Route path="/" exact component={Home} />
-          <Route path="/about/" component={About} />
-          <Route path="/status/" component={Status} />
+          <ul>
+            <li>
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/status">
+                Status
+              </Link>
+            </li>
+            <li>
+              {localStorage.getItem("token") ? (
+                  <button onClick={logout}>Logout</button>
+              ) : (
+                  <Link className="nav-link" to="/login">
+                    Login
+                  </Link>
+              )}
+            </li>
+          </ul>
+
+          <div>
+            <Route path="/" exact component={Home} />
+            <Route path="/about/" component={About} />
+            <Route path="/status/" component={Status} />
+            <Route path="/login/" component={Login} />
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
   );
 };
 
